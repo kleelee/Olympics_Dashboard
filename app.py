447e7356dcdbbc6c3805450b9a97257d31ce7aa1
kleelee <kylelee02@gmail.com>
2019-01-11 18:54:51 -0500

    SQLAlchemy Queries and Flask API Routes for country and Athlete medals

--- app.py
+++ app.py
@@ -1,17 +1,92 @@
 import pandas as pd
 import numpy as np
-from sqlalchemy import create_engine, func
+from sqlalchemy import *
+from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 import pymysql
 from config import sqlpass
 from flask import Flask
+import json
 
 df = pd.read_csv("athlete_events.csv")
 
 rds_connection_string = f"root:{sqlpass}@127.0.0.1/olympics_db?charset=utf8"
 engine = create_engine(f'mysql://{rds_connection_string}', encoding='utf-8')
 
-df.to_sql(name='athlete_events', con=engine, if_exists='append', index=False)
+#df.to_sql(name='athlete_events', con=engine, if_exists='append', index=False)
 
-pd.read_sql_query('select * from athlete_events', con=engine).head(100) 
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Athletes = Base.classes.athletes
 
+session = Session(engine)
+
+app = Flask(__name__)
+
+@app.route("/api/v1.0/gold_athletes")
+def gold_athletes():
+    """Return the athletes data as json"""
+    items = session.query(Athletes.Name, Athletes.Sex, Athletes.Team, Athletes.Sport, func.count(Athletes.Medal == "Gold")).\
+    filter_by(Medal="Gold").\
+    group_by(Athletes.Name).\
+    order_by(func.count(Athletes.Medal == "Gold").desc()).limit(100)
+
+    work = {'data': [
+         {'Name':x.Name, 'Sex':
+            x.Sex, 'Country' :x.Team, 'Sport': x.Sport, 'Gold Medals': x[4]}
+        for x in items
+       ]}
+
+    jsonified_data = json.dumps(work)
+    return jsonified_data
+
+@app.route("/api/v1.0/gold_countries")
+def gold_countries():
+    """Return the athletes data as json"""
+    medalg = session.query( Athletes.Team, func.count(Athletes.Medal)).\
+    filter(Athletes.Medal == "Gold").\
+    group_by(Athletes.Team).\
+    order_by(func.count(Athletes.Medal).desc()).all()
+
+    work = {'data': [
+         {'Country':x.Team,'Gold Medals': x[1]}
+        for x in medalg
+       ]}
+
+    jsonified_data = json.dumps(work)
+    return jsonified_data
+
+@app.route("/api/v1.0/silver_countries")
+def silver_countries():
+    """Return the athletes data as json"""
+    medalg = session.query( Athletes.Team, func.count(Athletes.Medal)).\
+    filter(Athletes.Medal == "Silver").\
+    group_by(Athletes.Team).\
+    order_by(func.count(Athletes.Medal).desc()).all()
+
+    work = {'data': [
+         {'Country':x.Team,'Silver Medals': x[1]}
+        for x in medalg
+       ]}
+
+    jsonified_data = json.dumps(work)
+    return jsonified_data
+
+@app.route("/api/v1.0/bronze_countries")
+def bronze_countries():
+    """Return the athletes data as json"""
+    medalg = session.query( Athletes.Team, func.count(Athletes.Medal)).\
+    filter(Athletes.Medal == "Bronze").\
+    group_by(Athletes.Team).\
+    order_by(func.count(Athletes.Medal).desc()).all()
+
+    work = {'data': [
+         {'Country':x.Team,'Bronze Medals': x[1]}
+        for x in medalg
+       ]}
+
+    jsonified_data = json.dumps(work)
+    return jsonified_data
+
+if __name__ == "__main__":
+    app.run(debug=True)
